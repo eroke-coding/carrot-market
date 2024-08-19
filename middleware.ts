d24d41d9bb706bfa438c59596a118ba477cd0512
honi <honi85@naver.com>
2024-08-19 21:01:09 +0900

    add middleware.js

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Route {
+  [key: string]: Boolean;
+}
+
+const publicOnlyUrls: Route = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-accout": true,
+};
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const isPublicUrl = publicOnlyUrls[request.nextUrl.pathname];
+
+  if (!session.id) {
+    if (!isPublicUrl) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (isPublicUrl) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
